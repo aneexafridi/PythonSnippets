c481894531115b566cb9c673a817d95cc208c12b
aneexafridi <aneesafridi50@gmail.com>
2024-05-15 16:42:35 +0500

    uda

--- Advance Python/Directory py/Directory_Theory_1.py
+++ Advance Python/Directory py/Directory_Theory_1.py
@@ -2,4 +2,4 @@
 # os module - This module is used to perform simple operation on directories.
 # This module represents operating system dependent functionality.
 # Syntax:--
-#          import os
+#          import os 
