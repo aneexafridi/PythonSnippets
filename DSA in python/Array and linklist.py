a9ccabd0851718d58d8b61f7d31dc97464a539b6
aneexafridi <aneesafridi50@gmail.com>
2024-07-27 00:20:40 +0500

    dsa

--- DSA in python/Array and linklist.py
+++ DSA in python/Array and linklist.py
@@ -10,3 +10,10 @@ numbers = array("i",[1,2,3,4])
 # print(numbers)
 for num in numbers:
     print(num)
+    
+print(numbers[0]) # gets the 1st element
+print(numbers[-1]) #gets last item
+print(numbers[-2]) #gets second to last item
+
+#search for the index of the value 10
+print(numbers.index(10))
