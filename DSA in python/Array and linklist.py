06153a9b76d29d34353969f708719883f9624d86
aneexafridi <aneesafridi50@gmail.com>
2024-07-27 00:17:32 +0500

    Create Array and linklist.py

---
+++ DSA in python/Array and linklist.py
@@ -0,0 +1,12 @@
+from array import array
+
+"""
+The general syntax for creating an array looks like this:
+
+variable_name = array(typecode,[elements])
+"""
+
+numbers = array("i",[1,2,3,4])
+# print(numbers)
+for num in numbers:
+    print(num)
